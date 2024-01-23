cbf8a37172ec1ddb176a2d0d65622b1bd7c4da66
Hiroe <hiroeiwamoto1@gmail.com>
2024-01-23 21:44:05 +0100

    range les codes

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -7,17 +7,25 @@ use Symfony\Component\Routing\Attribute\Route;
 
 class MainController extends AbstractController
 {
-/*indexページにするためにはRouteの中を"/"のみすにするだけ*/
-      #[Route("/home", name:"main_home")]
+    /*indexページにするためにはRouteの中を"/"のみすにするだけ*/
+    #[Route("/home", name: "main_home")]
     public function home()
     {
         return $this->render('main/home.html.twig');
     }
 
 
-      #[Route("/test", name:"main_test")]
+    #[Route("/test", name: "main_test")]
     public function test()
     {
-        return $this->render('main-test.html.twig');
+        $serie = [
+            "title" => "serie tableauの０番",
+            "year" => "2024 tableau１番",
+        ];
+        return $this->render('main-test.html.twig', [
+            "mySerieTableau" => $serie,
+            "autreVar" => 100228
+        ]);
+
     }
 }
